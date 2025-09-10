31528decd07bd6c317c359cc2f59e25783b59c1c
Farhan Ishrak Islam <niloy28@live.com>
2025-09-11 00:22:24 +0900

    Delete unneeded contexts

--- context/audio-context.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { createContext } from "react";
-
-export const AudioContext = createContext({
-	audio: "",
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	setAudio: (audio: string) => {},
-});
--- context/is-song-liked-context.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { createContext } from "react";
-
-export const IsSongLikedContext = createContext({
-	isLiked: false,
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	setIsLiked: (isLiked: boolean) => {},
-});
