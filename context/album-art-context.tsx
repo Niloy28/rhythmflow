980b396728ae2b967fc8a0543f3ca972afea0021
Farhan Ishrak Islam <niloy28@live.com>
2025-09-11 00:12:56 +0900

    Delete unneeded file

--- context/album-art-context.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { createContext } from "react";
-
-export const AlbumArtContext = createContext({
-	albumArt: "",
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	setAlbumArt: (albumArt: string) => {},
-});
