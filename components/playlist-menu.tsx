972ba5c8e9caed582d56f6986420cc76e6e18e91
Farhan Ishrak Islam <niloy28@live.com>
2025-09-21 04:22:49 +0900

    Add playlist creation dialog

--- components/playlist-menu.tsx
+++ components/playlist-menu.tsx
@@ -10,6 +10,18 @@ import {
 import { cn } from "@/lib/utils";
 import { Ellipsis, PlusIcon } from "lucide-react";
 import WatchLaterButton from "./watch-later-button";
+import {
+	Dialog,
+	DialogClose,
+	DialogContent,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from "./ui/dialog";
+import { Button } from "./ui/button";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
 
 const PlaylistMenu = ({
 	songID,
@@ -22,26 +34,56 @@ const PlaylistMenu = ({
 }) => {
 	// TODO: Fetch user playlists and populate the menu dynamically
 	return (
-		<DropdownMenu onOpenChange={onPlaylistMenuOpen}>
-			<DropdownMenuTrigger
-				className={cn(
-					"flex justify-center items-center bg-background rounded-lg",
-					className
-				)}
-			>
-				<Ellipsis />
-			</DropdownMenuTrigger>
-			<DropdownMenuContent>
-				<DropdownMenuLabel>Playlists</DropdownMenuLabel>
-				<DropdownMenuItem>
-					<WatchLaterButton songID={songID} />
-				</DropdownMenuItem>
-				<DropdownMenuSeparator />
-				<DropdownMenuItem className="hover:cursor-pointer flex gap-1 justify-between">
-					Create New Playlist <PlusIcon />
-				</DropdownMenuItem>
-			</DropdownMenuContent>
-		</DropdownMenu>
+		// Wrap in Dialog for "Create New Playlist" functionality
+		<Dialog>
+			<DropdownMenu onOpenChange={onPlaylistMenuOpen}>
+				<DropdownMenuTrigger
+					className={cn(
+						"flex justify-center items-center bg-background rounded-lg",
+						className
+					)}
+				>
+					<Ellipsis />
+				</DropdownMenuTrigger>
+				<DropdownMenuContent>
+					<DropdownMenuLabel>Playlists</DropdownMenuLabel>
+					<DropdownMenuItem>
+						<WatchLaterButton songID={songID} />
+					</DropdownMenuItem>
+					<DropdownMenuSeparator />
+					<DialogTrigger asChild>
+						<DropdownMenuItem className="hover:cursor-pointer flex gap-1 justify-between">
+							Create New Playlist <PlusIcon />
+						</DropdownMenuItem>
+					</DialogTrigger>
+				</DropdownMenuContent>
+			</DropdownMenu>
+
+			{/* Playlist creation dialog */}
+			<form action="">
+				<DialogContent>
+					<DialogHeader>
+						<DialogTitle>Create New Playlist</DialogTitle>
+					</DialogHeader>
+					<div className="grid gap-4">
+						<div className="grid gap-3">
+							<Label htmlFor="playlist-name">Name</Label>
+							<Input
+								id="playlist-name"
+								name="playlist-name"
+								defaultValue="New Playlist"
+							/>
+						</div>
+					</div>
+					<DialogFooter>
+						<DialogClose asChild>
+							<Button variant="outline">Cancel</Button>
+						</DialogClose>
+						<Button type="submit">Create</Button>
+					</DialogFooter>
+				</DialogContent>
+			</form>
+		</Dialog>
 	);
 };
 
